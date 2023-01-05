70a43f40e8508b28e5e3eae98c3a6059aadfab0e
Natanael J Rabello <natanaeljrabello@gmail.com>
2023-01-04 22:32:33 -0300

    serde_generate_target aditional fixes
    Signed-off-by: Natanael J Rabello <natanaeljrabello@gmail.com>

--- serde-cpp/serde_attr/cmake/serde-cpp.cmake
+++ serde-cpp/serde_attr/cmake/serde-cpp.cmake
@@ -1,77 +1,72 @@
 include(CMakeParseArguments)
 
 #########################################################################################
-# serde_generate_target(<target> [SUFFIX <suffix>]
-# SUFFIX default: "_serde.h"
+# Function to generate serde headers for source files.
+# serde_generate_target(<target> [SUFFIX <suffix>])
+# ARGS:
+#   SUFFIX default: "_serde.h"
+# TODO: pass suffix to serde_gen
 #########################################################################################
 function(serde_generate_target TARGET)
 
-  # Parse function arguments
+  # Parse arguments
   set(prefix ARG)
   set(flags)
   set(singleValues SUFFIX)
   set(multiValues)
-  cmake_parse_arguments(PARSE_ARGV 1
-    "${prefix}"
-    "${flags}"
-    "${singleValues}"
-    "${multiValues}")
+  cmake_parse_arguments(PARSE_ARGV 1 "${prefix}" "${flags}" "${singleValues}" "${multiValues}")
 
-  # Args validation and set default values
-  if(NOT DEFINED ARG_UNPARSED_ARGUMENTS)
+  # Args validation and default values
+  if(NOT DEFINED ARG_UNPARSED_ARGUMENTS OR ARG_UNPARSED_ARGUMENTS STREQUAL "")
     message(FATAL_ERROR "TARGET ${TARGET}: missing sources")
   endif()
-  if(NOT DEFINED ARG_SUFFIX)
+  set(ARG_SOURCES ${ARG_UNPARSED_ARGUMENTS})
+
+  if(NOT DEFINED ARG_SUFFIX OR ARG_SUFFIX STREQUAL "")
     set(ARG_SUFFIX "_serde.h")
   endif()
 
-  set(ARG_SOURCES ${ARG_UNPARSED_ARGUMENTS})
-
-  # Generate list of serde header file names
-  foreach(SOURCE IN LISTS ARG_SOURCES)
-    get_filename_component(FILEPATH ${SOURCE} ABSOLUTE)
-    cmake_path(IS_PREFIX CMAKE_CURRENT_SOURCE_DIR "${FILEPATH}" NORMALIZE IS_PREFIX)
+  # Generate new list of serde header file names
+  foreach(FILE IN LISTS ARG_SOURCES)
+    # Get file absolute path
+    get_filename_component(FILE_ABSPATH ${FILE} ABSOLUTE)
+    # Validate path within current source dir
+    cmake_path(IS_PREFIX CMAKE_CURRENT_SOURCE_DIR "${FILE_ABSPATH}" NORMALIZE IS_PREFIX)
     if(NOT IS_PREFIX)
-      message(FATAL_ERROR "${TARGET}: ${FILEPATH} is external to current directory")
+      message(FATAL_ERROR "${TARGET}: ${FILE} is external to current directory")
     endif()
-    cmake_path(RELATIVE_PATH FILEPATH OUTPUT_VARIABLE FILE_REALPATH)
-    #message(STATUS "FILE_REALPATH:${FILE_REALPATH}")
+    # Get file relative path
+    cmake_path(RELATIVE_PATH FILE_ABSPATH OUTPUT_VARIABLE FILE_REALPATH)
 
     # Add "_serde.h" suffix to source file
     string(REGEX REPLACE "(.+)[.][^.]+$" "\\1${ARG_SUFFIX}" SERDE_HEADER ${FILE_REALPATH})
     if("${SERDE_HEADER}" STREQUAL "${FILE_REALPATH}")
-      string(REGEX REPLACE "(.+)" "\\1${ARG_SUFFIX}" SERDE_HEADER ${FILE_REALPATH})
+      string(CONCAT SERDE_HEADER "${FILE_REALPATH}" "${ARG_SUFFIX}")
     endif()
     string(PREPEND SERDE_HEADER "${CMAKE_CURRENT_BINARY_DIR}/")
 
+    # Add file to control lists
     list(APPEND SERDE_HEADERS ${SERDE_HEADER})
-    list(APPEND SOURCES ${FILEPATH})
+    list(APPEND SOURCES ${FILE_ABSPATH})
   endforeach()
 
-  #message(STATUS "${TARGET}_SERDE_HEADERS: ${SERDE_HEADERS}")
-  #message(STATUS "${TARGET}_SOURCES: ${SOURCES}")
-
-  # Export targets' serde header to parent scope
+  # Export target's serde headers variable
   set("${TARGET}_SERDE_FILES" ${SERDE_HEADERS} PARENT_SCOPE)
-
-  # Pre-create the headers for libclang to find them upon #include when parsing the sources.
-  file(TOUCH ${SERDE_HEADERS})
 
   # Command to generate the serde headers
   add_custom_command(
     OUTPUT ${SERDE_HEADERS}
-    # TODO: pass suffix to serde_gen
-    COMMAND $<TARGET_FILE:serde_gen> --database_dir=${CMAKE_BINARY_DIR} --database_file=compile_commands.json ${SOURCES}
+    COMMAND touch ${SERDE_HEADERS} && $<TARGET_FILE:serde_gen> --database_dir=${CMAKE_BINARY_DIR} --database_file=compile_commands.json ${SOURCES}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     DEPENDS serde_gen ${SOURCES})
 
-  # Dependency target
-  add_custom_target("${TARGET}_dep" ALL DEPENDS ${SERDE_HEADERS})
+  # Build dependency target
+  add_custom_target("${TARGET}_custom" ALL DEPENDS ${SERDE_HEADERS})
 
-  # Final target with include dir set
+  # Final target with include dir exported
   add_library(${TARGET} INTERFACE IMPORTED)
   target_include_directories(${TARGET} INTERFACE ${CMAKE_CURRENT_BINARY_DIR})
-  add_dependencies(${TARGET} "${TARGET}_dep")
+  add_dependencies(${TARGET} "${TARGET}_custom")
 
 endfunction(serde_generate_target)
 
