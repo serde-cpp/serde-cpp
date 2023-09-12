8cf067efac0c74775897f1f8e721b5e12216a5e9
Natanael J Rabello <natanaeljrabello@gmail.com>
2023-09-12 18:07:17 -0300

    serde_gen: init simplify func calls
    Signed-off-by: Natanael J Rabello <natanaeljrabello@gmail.com>

--- serde-cpp/serde_gen/src/init.cpp
+++ serde-cpp/serde_gen/src/init.cpp
@@ -12,7 +12,8 @@
 
 namespace serde_gen::init {
 
-auto create_option_list() -> cxxopts::Options {
+auto create_option_list() -> cxxopts::Options
+{
     // clang-format off
     auto option_list = cxxopts::Options("serde_gen",
                                         "serde_gen - serde-cpp serialization and deserialization generator.\n");
@@ -36,8 +37,8 @@ auto create_option_list() -> cxxopts::Options {
     return option_list;
 }
 
-bool handle_help_version(const cxxopts::Options &option_list,
-                         const cxxopts::ParseResult &options) {
+bool handle_help_version(const cxxopts::Options& option_list, const cxxopts::ParseResult& options)
+{
     if (options.count("help")) {
         std::cout << option_list.help() << std::endl;
         return true;
@@ -50,7 +51,8 @@ bool handle_help_version(const cxxopts::Options &option_list,
     return false;
 }
 
-auto validate_options(const cxxopts::ParseResult &options) -> int {
+auto validate_options(const cxxopts::ParseResult& options) -> int
+{
     int ret = 0;
     if (!options.count("source") || options["source"].as<std::string>().empty()) {
         std::cerr << "missing --source argument\n";
@@ -63,7 +65,8 @@ auto validate_options(const cxxopts::ParseResult &options) -> int {
     return ret;
 }
 
-static auto create_clang_compilation_config(const cxxopts::ParseResult &options) {
+static auto create_clang_compilation_config(const cxxopts::ParseResult& options)
+{
     cppast::libclang_compile_config config;
     if (options.count("database_dir")) {
         auto db = cppast::libclang_compilation_database(options["database_dir"].as<std::string>());
@@ -76,28 +79,32 @@ static auto create_clang_compilation_config(const cxxopts::ParseResult &options)
     return config;
 }
 
-static auto add_compilation_flags(const cxxopts::ParseResult &_options,
-                                  cppast::libclang_compile_config &clang_cfg) {
+static auto add_compilation_flags(const cxxopts::ParseResult& _options,
+                                  cppast::libclang_compile_config& clang_cfg)
+{
     cppast::compile_flags flags;
     clang_cfg.set_flags(cppast::cpp_standard::cpp_17, flags);
 }
 
-static auto add_include_directories(const cxxopts::ParseResult &options,
-                                    cppast::libclang_compile_config &clang_cfg) {
+static auto add_include_directories(const cxxopts::ParseResult& options,
+                                    cppast::libclang_compile_config& clang_cfg)
+{
     if (options.count("include_directory")) {
-        for (auto &include: options["include_directory"].as<std::vector<std::string>>())
+        for (auto& include : options["include_directory"].as<std::vector<std::string>>())
             clang_cfg.add_include_dir(include);
     }
 }
 
-static auto init_clang_compilation_config(const cxxopts::ParseResult &options) {
+static auto init_clang_compilation_config(const cxxopts::ParseResult& options)
+{
     auto clang_cfg = create_clang_compilation_config(options);
     add_compilation_flags(options, clang_cfg);
     add_include_directories(options, clang_cfg);
     return clang_cfg;
 }
 
-static auto init_diagnostic_logger(const cxxopts::ParseResult &options) {
+static auto init_diagnostic_logger(const cxxopts::ParseResult& options)
+{
     cppast::stderr_diagnostic_logger logger;
     auto verbose = options.count("verbose");
     if (verbose)
@@ -105,7 +112,8 @@ static auto init_diagnostic_logger(const cxxopts::ParseResult &options) {
     return logger;
 }
 
-static auto touch_file(const std::string &filename) {
+static auto touch_file(const std::string& filename)
+{
     auto out_base_path = std::filesystem::path(filename).remove_filename();
     std::filesystem::create_directories(out_base_path);
     std::ofstream file(filename);
@@ -117,9 +125,10 @@ static auto touch_file(const std::string &filename) {
     return true;
 }
 
-static auto parse_source_to_ast(const cxxopts::ParseResult &options)
--> std::unique_ptr<cppast::cpp_file> {
-    const auto &source_filename = options["source"].as<std::string>();
+static auto parse_source_to_ast(const cxxopts::ParseResult& options)
+    -> std::unique_ptr<cppast::cpp_file>
+{
+    const auto& source_filename = options["source"].as<std::string>();
     const auto fatal_errors = options.count("fatal_errors");
     auto clang_cfg = init_clang_compilation_config(options);
     auto logger = init_diagnostic_logger(options);
@@ -127,9 +136,10 @@ static auto parse_source_to_ast(const cxxopts::ParseResult &options)
     return src_ast;
 }
 
-auto run_serde_generator(const cxxopts::ParseResult &options) -> int {
+auto run_serde_generator(const cxxopts::ParseResult& options) -> int
+{
     // Pre-create output file for handling #include of generated serde file while parsing source
-    const auto &output_filename = options["output"].as<std::string>();
+    const auto& output_filename = options["output"].as<std::string>();
     if (!touch_file(output_filename))
         return 3;
 
@@ -138,10 +148,10 @@ auto run_serde_generator(const cxxopts::ParseResult &options) -> int {
         return 2;
 
     if (options.count("verbose"))
-        serde_gen::print_ast(std::cout, *src_ast);
+        print_ast(std::cout, *src_ast);
 
     std::ofstream outfile(output_filename);
-    serde_gen::generate_serde_for_file(outfile, *src_ast);
+    generate_serde_for_file(outfile, *src_ast);
 
     return 0;
 }
